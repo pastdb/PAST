fc27bbfa588646604571939ff6b8f8770fe557b4
Nikos Kokolakis <kokolakisnikos@gmail.com>
2014-04-09 03:17:02 +0200

    script to upload scaladocs to github

---
+++ scripts/publish_api.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+die() {
+	echo "$@" 1>&2
+	exit 1
+}
+
+# make sure we are called from the project's root dir
+if [ ! -e build.sbt ]; then
+	die "Please run the script from the project's root directory."
+fi
+
+sbt doc || die "Documents could not be built."
+
+OLD_BRANCH=$(git branch | grep '^*' | sed -e 's/^* //')
+git checkout gh-pages || die "could not switch to gh-pages"
+git rm -rf api/
+cp -R target/scala-2.10/api ./api
+git add api/
+git commit -m "publish_api.sh generated docs"
+
+git checkout "$OLD_BRANCH" || die "Cannot switch back to ${OLD_BRANCH}."
+git push -u origin gh-pages
+
