770c32dd4abf7a7aa1737b4a4d35fe725ed63325
Nikos Kokolakis <kokolakisnikos@gmail.com>
2014-04-09 22:23:00 +0200

    include commit hash in publish_api.sh message

--- scripts/publish_api.sh
+++ scripts/publish_api.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 die() {
 	echo "$@" 1>&2
 	exit 1
@@ -12,12 +14,14 @@ fi
 
 sbt doc || die "Documents could not be built."
 
+COMMIT_HASH=$(git log -n1 --format="format:%H" HEAD)
 OLD_BRANCH=$(git branch | grep '^*' | sed -e 's/^* //')
+
 git checkout gh-pages || die "could not switch to gh-pages"
 git rm -rf api/
 cp -R target/scala-2.10/api ./api
 git add api/
-git commit -m "publish_api.sh generated docs"
+git commit -m "publish_api.sh generated docs for commit ${COMMIT_HASH}"
 
 git checkout "$OLD_BRANCH" || die "Cannot switch back to ${OLD_BRANCH}."
 git push -u origin gh-pages
