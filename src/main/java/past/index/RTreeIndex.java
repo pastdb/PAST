8ac8dbb15bd7d8341201e9e8866a9dbc3cbadc73
Eleni Tzirita Zacharatou <eleni.tzirita.zacharatou@gmail.com>
2014-05-13 03:36:14 +0200

    single imports in RTreeIndex

--- src/main/java/past/index/RTreeIndex.java
+++ src/main/java/past/index/RTreeIndex.java
@@ -1,7 +1,11 @@
 package past.index;
 
 import java.math.BigInteger;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.khelekore.prtree.DistanceResult;
